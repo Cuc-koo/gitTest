fa044cad3d307533aaf3eccaddc3ee2be2195fa2
Cuckoo <480533257@qq.com>
2022-08-05 02:10:47 +0800

    third commit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,5 +2,7 @@ public class HelloGit {
     public static void main (String[] args){
         System.out.printf("hello Git!");
         System.out.printf("hello Git!");
+        System.out.printf("hello Git!");
+
     }
 }
