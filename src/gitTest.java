ad0e43c19b99b8ee515b04edbf6482771e57129b
Cuckoo <480533257@qq.com>
2022-08-05 13:13:29 +0800

    master git test

---
+++ src/gitTest.java
@@ -0,0 +1,15 @@
+public class gitTest {
+    public static void main (String[] args){
+        System.out.printf("hello Git!");
+        System.out.printf("hello Git!");
+
+        System.out.printf("hello Git!");
+
+
+        System.out.printf("master test");
+
+
+
+
+    }
+}
